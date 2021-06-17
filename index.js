0525395519d24c1d028b2a7e41c73d0d2667fbb7
ridia13 <ridia2005@naver.com>
2021-06-17 23:09:39 +0900

    input hero name

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const $hero = document.querySelector("#js-hero");
+
+const getName = (event) => {//hero name 입력
+  event.preventDefault();
+  const target = event.target;
+  const name = target["hero-name"].value;
+}
+
+function init(){
+  $hero.addEventListener('submit', getName);//hero name 입력
+  //주인공 생성
+}
+
+init();
