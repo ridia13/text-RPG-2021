b89b4bd50e03373ff25d8081770e1ccbd82ffc2f
ridia13 <ridia2005@naver.com>
2021-06-19 16:53:56 +0900

    create hero

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 "use strict";
 
-const $hero = document.querySelector("#js-hero");
-
-const getName = (event) => {//hero name 입력
-  event.preventDefault();
-  const target = event.target;
-  const name = target["hero-name"].value;
+const $setNameForm = document.querySelector("#js-setName");
+const $gameModeForm = document.querySelector("#js-gameMode"),
+  $aboutHero = document.querySelector('#js-aboutHero');
+const initialHero = {
+  name: '',
+  level: 1,
+  hp: 100,
+  maxHp: 100,
+  xp: 0,
+  att: 10
 }
 
-function init(){
-  $hero.addEventListener('submit', getName);//hero name 입력
+const gameMode = () => {
+  $aboutHero.textContent = `Name : ${initialHero.name}, Level : ${initialHero.level}, Hp : ${initialHero.hp} / ${initialHero.maxHp}, Xp : ${initialHero.xp} / ${initialHero.level* 15} , Att : ${initialHero.att} `;
+  $gameModeForm.style.display = "block";
+}
+
+const getName = (event) => { //hero name 입력
+  event.preventDefault();
+  const target = event.target;
+  initialHero.name = target["hero-name"].value;
+  $setNameForm.style.display = "none";
+  gameMode();
+}
+
+
+
+function init() {
+  $setNameForm.addEventListener('submit', getName); //hero name 입력
   //주인공 생성
 }
 
