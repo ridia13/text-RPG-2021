68a3047b501c905114550d80888dea5839d2467b
ridia13 <ridia2005@naver.com>
2021-06-23 16:53:38 +0900

    constructor ver

--- index.js
+++ index.js
@@ -5,23 +5,28 @@ const $aboutHero = document.querySelector('#js-aboutHero');
 const $gameModeForm = document.querySelector("#js-gameMode");
 const $battleModeForm = document.querySelector("#js-battleMode"),
   $aboutMonster = $battleModeForm.querySelector('#js-aboutMonster');
-const $exitMsg = document.querySelector("#js-exitMsg");
+const $msg = document.querySelector("#js-msg");
 const initialHero = { //주인공 생성
   name: '',
   level: 1,
   hp: 100,
   maxHp: 100,
   xp: 0,
-  att: 10
+  att: 10,
+  attack(monster) {
+    monster.hp -= this.att;
+    this.hp -= monster.att;
+  }
 }
-
-function Monster(name, level) { //monster 생성 
-  this.name = name;
-  this.level = level;
-  this.hp = this.level * 20;
-  this.maxHp = this.hp;
-  this.att = this.level * 2;
-  this.xp = this.att + 5;
+class Monster {
+  constructor(name, level) { //monster 생성 
+    this.name = name;
+    this.level = level;
+    this.hp = this.level * 20;
+    this.maxHp = this.hp;
+    this.att = this.level * 2;
+    this.xp = this.att + 5;
+  }
 }
 const monster1 = new Monster('GreenSnail', 5);
 const monster2 = new Monster('Slime', 10);
@@ -57,48 +62,65 @@ const handleGameMode = (event) => {
     initialHero.hp = initialHero.maxHp;
   } else if (menuBtn.id === '3') { //3.end
     $gameModeForm.style.display = "none";
-    $exitMsg.style.display = "block";
+    $msg.textContent = "Exit the game.";
+    $msg.style.display = "block";
   }
   $aboutHero.textContent = `${initialHero.name}, Level${initialHero.level}, HP: ${initialHero.hp}/${initialHero.maxHp}, XP: ${initialHero.xp}/${initialHero.level* 15} , Att: ${initialHero.att} `;
 }
 
 const checkHp = (turn, other) => {
-  if(other.hp === 0){
+  if (other.hp === 0) {
     console.log(`${turn}win`);
   }
   heroTurn = false;
-  if(heroTurn)return;
-  setInterval(() => {console.log(`1`)},2000);
+  if (heroTurn) return;
+  setInterval(() => {
+    console.log(`1`)
+  }, 2000);
 }
 let heroTurn = true;
+
+const runAway = (text, ms) => {
+  $msg.textContent = text;
+  $msg.style.display = "block";
+  setTimeout(() => {
+    $msg.style.display = "none";
+    $battleModeForm.style.display = "none";
+    $gameModeForm.style.display = "block";
+  }, ms);
+}
+
+const monsterTurn = () => { //monster 공격(timeOut)
+  heroTurn = false;
+  setTimeout(() => {
+    initialHero.hp -= currentMonster.att;
+    $aboutHero.textContent = `${initialHero.name}, Level${initialHero.level}, HP: ${initialHero.hp}/${initialHero.maxHp}, XP: ${initialHero.xp}/${initialHero.level* 15} , Att: ${initialHero.att} `;
+    if (initialHero.hp <= 0) { //졌을 경우
+      runAway(`I lost to a ${currentMonster.name}. Let's run away!!`, 3000);
+    }
+    heroTurn = true;
+  }, 1000);
+}
+
 const handleBattleMode = (event) => {
   event.preventDefault();
   const menuBtn = event.target;
+  if (!heroTurn) return; //monster turn일 경우
+  //hero turn
   if (menuBtn.id === '1') { //1.공격
-    console.log(heroTurn);
-    if(!heroTurn)return;
-    currentMonster.hp -= initialHero.att;//내 공격
-    if(currentMonster.hp <= 0){
-      if(heroTurn){
-        console.log(`hero 승리`);
-        initialHero.xp += currentMonster.xp;
-      }else{
-        console.log(`패배`);
-      }
-    }else{
-      heroTurn = false;
-      if(heroTurn)return;
-      setTimeout(() => {
-        console.log(`time`);
-        initialHero.hp -= currentMonster.att;//적 공격
-      }, 2000);
+    currentMonster.hp -= initialHero.att; //hero 공격
+    if (currentMonster.hp <= 0) { //이겼을 경우
+      $msg.textContent = "Win!!";
+      $msg.style.display = "block";
+      initialHero.xp += currentMonster.xp;
+    } else if (currentMonster.hp > 0) {
+      monsterTurn();
     }
-    
   } else if (menuBtn.id === '2') { //2.회복
     initialHero.hp = initialHero.maxHp;
+    monsterTurn();
   } else if (menuBtn.id === '3') { //3.도망
-    $battleModeForm.style.display = "none";
-    $gameModeForm.style.display = "block";
+    runAway("Let's run away!", 1000);
   }
   $aboutMonster.textContent = `${currentMonster.name}, HP: ${currentMonster.hp}/${currentMonster.maxHp}`;
   $aboutHero.textContent = `${initialHero.name}, Level${initialHero.level}, HP: ${initialHero.hp}/${initialHero.maxHp}, XP: ${initialHero.xp}/${initialHero.level* 15} , Att: ${initialHero.att} `;
